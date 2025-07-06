3a6ab216bebeb96ccbdced7d19424eec083c4fd4
BHANUJA-K <bhanujakumaresanar@gmail.com>
2025-07-06 18:55:52 +0530

    Add files via upload

---
+++ Week 3 - Spring Core Maven/Excercise 2/code/BookRepository.java
@@ -0,0 +1,10 @@
+package com.library.repository;
+
+public class BookRepository {
+    public void getAllBooks() {
+        System.out.println("[Repository] Fetching all books:");
+        System.out.println(" - 'Clean Code' by Robert C. Martin");
+        System.out.println(" - 'Effective Java' by Joshua Bloch");
+        System.out.println(" - 'Spring in Action' by Craig Walls");
+    }
+}
